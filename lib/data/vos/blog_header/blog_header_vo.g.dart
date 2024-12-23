26bf030a4a7b773e0bb5088bb6074cc0791739d8
Thiha Thant Sin <42347119+ThihaThantSin777@users.noreply.github.com>
2024-12-24 03:13:21 +0700

    change type list

--- lib/data/vos/blog_header/blog_header_vo.g.dart
+++ lib/data/vos/blog_header/blog_header_vo.g.dart
@@ -11,7 +11,8 @@ BlogHeaderVO _$BlogHeaderVOFromJson(Map<String, dynamic> json) => BlogHeaderVO(
       title: json['Title'] as String,
     );
 
-Map<String, dynamic> _$BlogHeaderVOToJson(BlogHeaderVO instance) => <String, dynamic>{
+Map<String, dynamic> _$BlogHeaderVOToJson(BlogHeaderVO instance) =>
+    <String, dynamic>{
       'Id': instance.id,
       'Title': instance.title,
     };
