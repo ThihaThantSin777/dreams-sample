33eec820aea572c2bf4fc1fb2ebbf970f988c766
Thiha Thant Sin <42347119+ThihaThantSin777@users.noreply.github.com>
2024-12-24 03:29:01 +0700

    adjustment vo and response keys

--- lib/data/vos/blog_header/blog_header_vo.dart
+++ lib/data/vos/blog_header/blog_header_vo.dart
@@ -5,13 +5,19 @@ part 'blog_header_vo.g.dart';
 ///Blog Header attribute အတွက် VO class
 @JsonSerializable()
 class BlogHeaderVO {
-  @JsonKey(name: 'Id')
+  @JsonKey(name: 'BlogId')
   final int id;
 
-  @JsonKey(name: 'Title')
+  @JsonKey(name: 'BlogTitle')
   final String title;
 
-  BlogHeaderVO({required this.id, required this.title});
+  String? prefixWord;
+
+  BlogHeaderVO({
+    required this.id,
+    required this.title,
+    this.prefixWord,
+  });
 
   factory BlogHeaderVO.fromJson(Map<String, dynamic> json) => _$BlogHeaderVOFromJson(json);
 
